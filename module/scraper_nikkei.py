3120a7c9fb68f667fb13629807b5cc07802a8e0b
madoake <zodo0209@yahoo.co.jp>
2024-07-20 17:17:45 +0900

    step1.・各リンクにアクセスして タイトル、日付、内容を標準出力

--- module/scraper_nikkei.py
+++ module/scraper_nikkei.py
@@ -1,6 +1,7 @@
 from libs.soup_util import RequestsUtil as req
 from libs.db_util import HrInfo, Database
 
+
 BASE_URL = 'https://www.nikkei.com'  # ベースURLの定義
 
 class ScraperNikkei():
@@ -11,8 +12,8 @@ class ScraperNikkei():
         info_urls = self.get_urls(BASE_URL + '/news/jinji/hatsurei/')
 
         # 2.記事ページからタイトル、日付、内容をDBに保存する
-        #for url in info_urls:
-        #    self.get_content(url)
+        for url in info_urls:
+            self.get_content(url)
 
         self.session.close()
 
@@ -37,5 +38,11 @@ class ScraperNikkei():
         # 記事ページからタイトル、日時、内容を取得する処理
         # Step1 標準出力しよう
         # Step2 SQLAlchemyでデータベースのhr_infoテーブルに格納しよう
+        title=soup.find('h1', class_='title_t1xgcrem').get_text()
+        date=soup.find('time').get_text()
+        content=soup.find('p', class_='paragraph_p18mfke4').get_text()
+        print(f"タイトル: {title}")
+        print(f"日付: {date}")
+        print(f"内容: {content}")
 
         self.session.commit()
